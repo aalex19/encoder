ddce1bee7622a5dd1eda45ad6c900ed07d1b8016
aalex19 <123383724+aalex19@users.noreply.github.com>
2023-03-20 22:18:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode\n2. Decode\n3. Quit" )
+
+def encode(password):
+    encode = ""
+    for pas in password:
+        pas = int(pas) + 3
+        encode += str(pas)
+    return encode
+
+while True:
+    menu()
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        encode(password)
+        print("Your password has been encoded and stored!")
+
+    #if option == 2:
+
+    if option == 3:
+        quit()
