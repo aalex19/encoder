4f84b0779ef3d59e820a72c76e384cbe84b2d688
justinesmith <123383763+justinesmith@users.noreply.github.com>
2023-03-22 23:24:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,27 @@ def encode(password):         #encodes user input by adding 3 to each value of p
         encode += str(pas)
     return encode
 
+
+def password_decoder(new_password):
+    old_password = ''
+    for i in new_password:
+        old_char = str((int(i)-3) % 10)
+        old_password += old_char
+    return old_password
+
+
 while True:
     menu()
     option = int(input("Please enter an option: "))
     if option == 1:
         password = input("Please enter your password to encode: ")
-        encode(password)
+        encoded_password = encode(password)
         print("Your password has been encoded and stored!")
 
-    #if option == 2:
+    if option == 2:
+        decoded_password = password_decoder(str(encoded_password))
+    print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+    program_on = True
 
     if option == 3:               #quit program
         quit()
